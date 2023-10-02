fece1a1d705aa2b846a35bedabdd11761505f95c
greg <lecoboy@163.com>
2023-10-02 15:25:28 +0800

    save attr with relation

--- gulimall-product/src/main/java/com/leco/gulimall/product/service/AttrService.java
+++ gulimall-product/src/main/java/com/leco/gulimall/product/service/AttrService.java
@@ -3,6 +3,7 @@ package com.leco.gulimall.product.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.leco.gulimall.common.utils.PageUtils;
 import com.leco.gulimall.product.entity.AttrEntity;
+import com.leco.gulimall.product.vo.AttrVO;
 
 import java.util.Map;
 
@@ -16,5 +17,7 @@ import java.util.Map;
 public interface AttrService extends IService<AttrEntity> {
 
     PageUtils queryPage(Map<String, Object> params);
+
+    void saveAttr(AttrVO attr);
 }
 
--- gulimall-product/src/main/java/com/leco/gulimall/product/service/impl/AttrServiceImpl.java
+++ gulimall-product/src/main/java/com/leco/gulimall/product/service/impl/AttrServiceImpl.java
@@ -1,20 +1,28 @@
 package com.leco.gulimall.product.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.leco.gulimall.common.utils.PageUtils;
 import com.leco.gulimall.common.utils.Query;
-
 import com.leco.gulimall.product.dao.AttrDao;
+import com.leco.gulimall.product.entity.AttrAttrgroupRelationEntity;
 import com.leco.gulimall.product.entity.AttrEntity;
+import com.leco.gulimall.product.service.AttrAttrgroupRelationService;
 import com.leco.gulimall.product.service.AttrService;
+import com.leco.gulimall.product.vo.AttrVO;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Map;
 
 
 @Service("attrService")
 public class AttrServiceImpl extends ServiceImpl<AttrDao, AttrEntity> implements AttrService {
+
+    @Resource
+    private AttrAttrgroupRelationService relationDao;
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
@@ -24,6 +32,20 @@ public class AttrServiceImpl extends ServiceImpl<AttrDao, AttrEntity> implements
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public void saveAttr(AttrVO attr) {
+        AttrEntity attrEntity = new AttrEntity();
+        BeanUtils.copyProperties(attr, attrEntity);
+        //1、保存基本数据
+        this.save(attrEntity);
+
+        //2、保存关联关系
+        AttrAttrgroupRelationEntity relationEntity = new AttrAttrgroupRelationEntity();
+        relationEntity.setAttrGroupId(attr.getAttrGroupId());
+        relationEntity.setAttrId(attrEntity.getAttrId());
+        relationDao.save(relationEntity);
     }
 
 }
