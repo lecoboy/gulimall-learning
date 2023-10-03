6d501e76b37473414cf2024a42255a979ed62cbb
greg <lecoboy@163.com>
2023-10-03 14:59:22 +0800

    list attrGroup with attrs

--- gulimall-product/src/main/java/com/leco/gulimall/product/controller/AttrGroupController.java
+++ gulimall-product/src/main/java/com/leco/gulimall/product/controller/AttrGroupController.java
@@ -9,6 +9,7 @@ import com.leco.gulimall.product.service.AttrGroupService;
 import com.leco.gulimall.product.service.AttrService;
 import com.leco.gulimall.product.service.CategoryService;
 import com.leco.gulimall.product.vo.AttrGroupRelationVO;
+import com.leco.gulimall.product.vo.AttrGroupWithAttrsVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -50,6 +51,19 @@ public class AttrGroupController {
         PageUtils page = attrGroupService.queryPage(params, catelogId);
 
         return R.ok().put("page", page);
+    }
+
+    ///product/attrgroup/{catelogId}/withattr
+    //获取分类下所有分组&关联属性
+    @GetMapping(value = "/{catelogId}/withattr")
+    public R getAttrGroupWithAttrs(@PathVariable("catelogId") Long catelogId) {
+
+        //1、查出当前分类下的所有属性分组
+        //2、查出每个属性分组下的所有属性
+        List<AttrGroupWithAttrsVO> vos = attrGroupService.getAttrGroupWithAttrsByCatelogId(catelogId);
+
+        return R.ok().put("data",vos);
+
     }
 
     /**
--- gulimall-product/src/main/java/com/leco/gulimall/product/service/impl/AttrGroupServiceImpl.java
+++ gulimall-product/src/main/java/com/leco/gulimall/product/service/impl/AttrGroupServiceImpl.java
@@ -1,7 +1,16 @@
 package com.leco.gulimall.product.service.impl;
 
+import com.leco.gulimall.product.entity.AttrEntity;
+import com.leco.gulimall.product.service.AttrService;
+import com.leco.gulimall.product.vo.AttrGroupWithAttrsVO;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -16,6 +25,8 @@ import org.springframework.util.StringUtils;
 
 @Service("attrGroupService")
 public class AttrGroupServiceImpl extends ServiceImpl<AttrGroupDao, AttrGroupEntity> implements AttrGroupService {
+    @Autowired
+    private AttrService attrService;
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
@@ -45,6 +56,24 @@ public class AttrGroupServiceImpl extends ServiceImpl<AttrGroupDao, AttrGroupEnt
         }
 
         return new PageUtils(this.page(new Query<AttrGroupEntity>().getPage(params), wrapper));
+    }
+
+    @Override
+    public List<AttrGroupWithAttrsVO> getAttrGroupWithAttrsByCatelogId(Long catelogId) {
+        //1、查询分组信息
+        List<AttrGroupEntity> attrGroupEntities = this.list(new QueryWrapper<AttrGroupEntity>().eq("catelog_id", catelogId));
+
+        //2、查询所有属性
+
+        return attrGroupEntities.stream().map(group -> {
+            AttrGroupWithAttrsVO attrGroupWithAttrsVo = new AttrGroupWithAttrsVO();
+            BeanUtils.copyProperties(group,attrGroupWithAttrsVo);
+
+            List<AttrEntity> attrs = attrService.getRelationAttr(attrGroupWithAttrsVo.getAttrGroupId());
+            attrGroupWithAttrsVo.setAttrs(attrs);
+
+            return attrGroupWithAttrsVo;
+        }).collect(Collectors.toList());
     }
 
 }
