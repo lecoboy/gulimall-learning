eea876f0bbc5d342db20f709a3f72bda0414b355
greg <lecoboy@163.com>
2023-10-04 17:07:27 +0800

    list spu

--- gulimall-product/src/main/java/com/leco/gulimall/product/service/SpuInfoService.java
+++ gulimall-product/src/main/java/com/leco/gulimall/product/service/SpuInfoService.java
@@ -19,5 +19,7 @@ public interface SpuInfoService extends IService<SpuInfoEntity> {
     PageUtils queryPage(Map<String, Object> params);
 
     void saveSpuInfo(SpuSaveVO vo);
+
+    PageUtils queryPageByCondtion(Map<String, Object> params);
 }
 
