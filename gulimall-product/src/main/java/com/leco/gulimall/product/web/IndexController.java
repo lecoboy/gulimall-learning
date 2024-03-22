8d64557d4c67806ead5782a2abba4746c2f74e16
greg <lecoboy@163.com>
2024-03-22 16:29:50 +0800

    redisson lock

--- gulimall-product/src/main/java/com/leco/gulimall/product/web/IndexController.java
+++ gulimall-product/src/main/java/com/leco/gulimall/product/web/IndexController.java
@@ -3,7 +3,11 @@ package com.leco.gulimall.product.web;
 import com.leco.gulimall.product.entity.CategoryEntity;
 import com.leco.gulimall.product.service.CategoryService;
 import com.leco.gulimall.product.vo.Catelog2Vo;
+import org.redisson.api.RLock;
+import org.redisson.api.RReadWriteLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +15,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author greg
@@ -20,6 +26,12 @@ import java.util.Map;
 public class IndexController {
     @Autowired
     private CategoryService categoryService;
+
+    @Autowired
+    private RedissonClient redisson;
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
     @GetMapping(value = {"/", "index.html"})
     private String indexPage(Model model) {
@@ -39,5 +51,61 @@ public class IndexController {
 
         return categoryService.getCatalogJson();
 
+    }
+
+    @ResponseBody
+    @GetMapping("/hello")
+    public String hello() {
+        RLock lock = redisson.getLock("my-lock");
+        lock.lock();
+        try {
+            System.out.println("加锁成功，执行业务...");
+            try { TimeUnit.SECONDS.sleep(20); } catch (InterruptedException e) { e.printStackTrace(); }
+        } finally {
+            lock.unlock();
+        }
+
+        return "hello";
+    }
+
+    @GetMapping(value = "/write")
+    @ResponseBody
+    public String writeValue() {
+        String s = "";
+        RReadWriteLock readWriteLock = redisson.getReadWriteLock("rw-lock");
+        RLock rLock = readWriteLock.writeLock();
+        try {
+            //改数据加写锁，读数据加读锁
+            rLock.lock();
+            s = UUID.randomUUID().toString();
+            stringRedisTemplate.opsForValue().set("writeValue",s);
+            TimeUnit.SECONDS.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            rLock.unlock();
+        }
+
+        return s;
+    }
+
+    @GetMapping(value = "/read")
+    @ResponseBody
+    public String readValue() {
+        String s = "";
+        RReadWriteLock readWriteLock = redisson.getReadWriteLock("rw-lock");
+        //加读锁
+        RLock rLock = readWriteLock.readLock();
+        try {
+            rLock.lock();
+            s = stringRedisTemplate.opsForValue().get("writeValue");
+            try { TimeUnit.SECONDS.sleep(10); } catch (InterruptedException e) { e.printStackTrace(); }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            rLock.unlock();
+        }
+
+        return s;
     }
 }
